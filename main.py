9dfee378ddf8d38148468b61265bcf2aa9df4484
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-28 17:02:34 +0000

    adding 1 or 2 each move

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
-sticks = int(13)
+score = 0 
+def Start(): 
+  PlayerOne = str(input(" Choose the first number..."))
+  if PlayerOne == "first":
+    print("PlayerOne starts")
 
-playerState = int(0)
+def Add():
+  s = 0
+  PlayerMove = str(input("add one or two each move..."))
+  if PlayerMove == "1":
+    s += 1
+    print(score)
+  if PlayerMove == "2": 
+    s += 2 
+    print(score)
+  return s
 
-#game mechanics
+Start()
 
+while score < 20:
+  score = score + Add()
 
-while (sticks > 0):
-    if (playerState %2 != 1):
-  playerMove = int(raw_input("Player One, please enter your move:"))
-    if (playerMove > 0) and (playerMove < 5) and
-(playerMove <= sticks):
-sticks = sticks - playerMove
-playerState +=1
-    		print (sticks)
-    		print (playerState)
-    	else:
-        	print("Illegal move.  Try again.")
-    else:
-    playerMove = int(raw_input("Player Two, please enter your move:"))
-    	if (playerMove > 0) and (playerMove < 5) and(playerMove <= sticks):
-    		sticks = sticks - playerMove
-    		playerState +=1
-    		print(sticks)
-    		print(playerState)
-        
-winner = int(playerState % 2)
-
-if (winner != 1):
-    print("Congratulations player 1")
-else:
-    print("Congratulations player 2")
-                                                      
